48169a04b5c034c48e0aab779ed9ab77161d6cb7
austin espinal <austinespi@gmail.com>
2022-04-05 14:36:54 -0400

    added servers component and some styling to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // inline style. use if only have few styling, else use external file. example below
+  // styles: [`
+  // h3 {
+  //   color: red
+  // }
+  // `]
 })
 export class AppComponent {
 }
