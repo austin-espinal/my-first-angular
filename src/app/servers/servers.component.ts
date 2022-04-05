c0dbc4c43483474333561195b13beae90bb08e9b
austin espinal <austinespi@gmail.com>
2022-04-05 15:55:23 -0400

    added comments on templating and selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // use selector by attribute. see below
+  // selector: '[app-servers]',
+  // use selector by class. can not use by ID or pseudo classes(hover). see below
+  // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  // can use template with just element instead of external file. see below
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
